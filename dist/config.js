1cdf73c3b2884d6236f4cfc70aafb39b3c0d27d0
mac <gos360@163.com>
2018-02-04 11:53:04 +0800

    ignore something

--- dist/config.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    cookieName: 'hehehehehehe',
-    mongodbUrl: 'mongodb://localhost:27017/myapp',
-    admin: 'admin',
-    sessionSecret: 'hahahahhahaha'
-};
-//# sourceMappingURL=config.js.map
